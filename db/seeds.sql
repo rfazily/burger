38ee79b4a7ff94aeef34ac492077cba832170195
Rayhana <rfazily@gmail.com>
2020-04-30 00:47:48 -0400

    adding all new handlebar and js files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+--inserting a set of values into table
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('In-n-Out', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shake Shack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
